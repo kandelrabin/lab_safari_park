091bb35cdcc146302d9be29c3de6660f0de216e6
Sunny Mudhar <sunielmudhar@gmail.com>
2024-05-23 15:03:58 +0100

    Inserted data into tables

--- safari.sql
+++ safari.sql
@@ -3,14 +3,11 @@ DROP TABLE IF EXISTS animal;
 DROP TABLE IF EXISTS staff;
 DROP TABLE IF EXISTS enclosure;
 
-
-
 CREATE TABLE staff(
     id SERIAL PRIMARY KEY,
     name VARCHAR(225),
     employee_number INT
 );
-
 
 CREATE TABLE enclosure(
     id SERIAL PRIMARY KEY,
@@ -18,7 +15,6 @@ CREATE TABLE enclosure(
     capacity INT,
     closed_for_maintenance BOOLEAN
 );
-
 
 CREATE TABLE animal(
     id SERIAL PRIMARY KEY,
@@ -35,5 +31,17 @@ CREATE TABLE assignment(
     enclosure_id INTEGER REFERENCES enclosure(id)
 );
 
+INSERT INTO staff (name, employee_number) VALUES ('Sunny', 0121);
+INSERT INTO staff (name, employee_number) VALUES ('Zuhair', 0161);
+INSERT INTO staff (name, employee_number) VALUES ('Rabin', 0200);
 
+INSERT INTO enclosure (name, capacity, closed_for_maintenance) VALUES ('Lion Enclosure', 2, FALSE);
+INSERT INTO enclosure (name, capacity, closed_for_maintenance) VALUES ('Penguin Enclosure', 3, TRUE);
+INSERT INTO enclosure (name, capacity, closed_for_maintenance) VALUES ('Zebra Enclosure', 1, FALSE);
 
+INSERT INTO animal (name, type, age, enclosure_id) VALUES ('Alex', 'Lion', 23, 1);
+INSERT INTO animal (name, type, age, enclosure_id) VALUES ('Skipper', 'Penguin', 16, 2);
+
+INSERT INTO assignment (day, employee_id, enclosure_id) VALUES ('Monday', 2, 2);
+INSERT INTO assignment (day, employee_id, enclosure_id) VALUES ('Monday', 1, 1);
+INSERT INTO assignment (day, employee_id, enclosure_id) VALUES ('Monday', 3, 3);
