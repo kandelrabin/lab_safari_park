b46beca19fe174bcb969088b3becaffa9dc23580
Rabin Kandel <kandelrabin@live.com>
2024-05-23 14:52:36 +0100

    complete animal and assignment

--- safari.sql
+++ safari.sql
@@ -1,7 +1,12 @@
+DROP TABLE IF EXISTS assignment;
+DROP TABLE IF EXISTS animal;
 DROP TABLE IF EXISTS staff;
+DROP TABLE IF EXISTS enclosure;
+
+
 
 CREATE TABLE staff(
-   id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(225),
     employee_number INT
 );
@@ -12,6 +17,22 @@ CREATE TABLE enclosure(
     name VARCHAR(225),
     capacity INT,
     closed_for_maintenance BOOLEAN
+);
+
+
+CREATE TABLE animal(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    type VARCHAR(255),
+    age INT,
+    enclosure_id INTEGER REFERENCES enclosure(id)
+);
+
+CREATE TABLE assignment(
+    id SERIAL PRIMARY KEY,
+    day VARCHAR(255),
+    employee_id INTEGER REFERENCES staff(id),
+    enclosure_id INTEGER REFERENCES enclosure(id)
 );
 
 
